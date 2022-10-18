f8ee1fa891925fb53f48e0b91276d500fe4bd7c5
AnanyaBhat2604 <ananya.bhat@robosoftin.com>
2022-10-18 10:26:44 +0530

    Next push

--- test/src/Views/landingPage/landingPage.tsx
+++ test/src/Views/landingPage/landingPage.tsx
@@ -3,12 +3,15 @@ import Lock from "../../Components/login/lock/lock"
 import { Routes, Route, Link } from "react-router-dom";
 import '../../Components/login/lockInput/lockInput.css';
 import SignUp from "../../Components/signUp/signUp"
+import HomePage from "../homePage/homePage";
 
 const LandingPage = () => {
     return (
         <div><Routes>
         <Route path="/" element={<Lock />} />
         <Route path="/signUp" element={<SignUp />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/*" element={<Lock />} />
       </Routes></div>
     )
 }
